c6446d6cbbac1794b77be724a8ad95cc161114ad
Alessandro Nardin <ale.daluch@gmail.com>
2026-06-25 17:58:51 +0200

    corrected multirate/handling by also zeroing e and setting diagonal element in R_eff to high value.

--- functions/EFK/ekf_step.m
+++ functions/EFK/ekf_step.m
@@ -16,27 +16,35 @@ function [xcurr, Pcurr, L, e] = ekf_step(xprev, Pprev, uprev, ucurr, z, plant_pa
     % innovation
     e = z - suspension_h(xpred, ucurr, [0; 0], plant_param);
     
-    % DETECTION DI OUTLIER (MAHALANOBIS) E MULTIRATE CONTROL
-    % Soglia per la distanza di Mahalanobis (impostata a 6 per i 6-sigma)
-    soglia = 6.0; 
+    % Outlier detection and Multirate handling
+    
+    % Mahalanobis threshold (6 for 6-sigma)
+    mhlb_th = 6.0; 
     
     for i = 1:size(z, 1)
-        % 1. OUTLIER DETECTION: valuto solo se il sensore ha inviato un dato (valid_meas == 1)
+        % Control on new measurements to detect outliers
         if valid_meas(i) == 1
-            % Calcolo l'incertezza S per la singola i-esima misura
+
+            % uncertainty
             S_ii = H(i, :) * Ppred * H(i, :)' + R_eff(i, i);
             
-            % Distanza di Mahalanobis: D_k = sqrt(e_k^T * S_k^-1 * e_k)
+            % Mahalanobis distance
             D_k = sqrt((e(i)^2) / S_ii);
             
-            % Se la distanza supera la soglia, è un outlier
-            if D_k > soglia
-                valid_meas(i) = 0; % Misura spuria: forzo valid_meas a 0 per scartarla
+            
+            if D_k > mhlb_th
+                valid_meas(i) = 0; % If outlier set to invalid
             end
         end
-        % 2. MULTIRATE CONTROL: annulla H se il sensore è lento o scartato per outlier
+        
+        % Remove effect of discarded measurements (outliers or old)
+      
         if valid_meas(i) == 0
-            H(i, :) = 0; % Annulla la riga dello Jacobiano per il sensore non aggiornato
+            % Set rows to zero to delete measurement effect
+            H(i, :) = 0;
+            e(i) = 0;
+            % Set corresponding value in R high value to delete effect
+            R_eff(i,i) = 1e12;
         end
     end
     
